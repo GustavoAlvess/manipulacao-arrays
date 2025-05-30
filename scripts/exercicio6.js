d060300fbb07879a8f15413cb9708867aeeb404a
GustavoAlvess <gustavo.a.lima21@aluno.senai.br>
2025-05-30 11:29:46 -0300

    feat: resolve exercicio 6

---
+++ scripts/exercicio6.js
@@ -0,0 +1,20 @@
+/* 🔹 6 – Primeira mensagem indevida
+Uma mensagem indevida foi enviada por engano no início do chat e deve ser removida da conversa para análise posterior.
+*/
+
+let mensagens = [
+    "Propaganda indevida",
+    "Bom dia, posso ajudar?",
+    "Preciso de suporte técnico",
+    "Claro, qual o problema?",
+    "Erro ao acessar conta"
+  ];
+
+  console.log(mensagens);
+  
+  mensagens.unshift() // removendo a imagem indevida
+  
+  console.log('Lista atualizada');
+  console.log(mensagens);
+
+  
