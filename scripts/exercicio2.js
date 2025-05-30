792a0c63117a52b14d47c936c5ddd1a7e6433681
GustavoAlvess <gustavo.a.lima21@aluno.senai.br>
2025-05-30 11:14:23 -0300

    feat: resolve exercicio 2

---
+++ scripts/exercicio2.js
@@ -0,0 +1,19 @@
+/*2 – Expansão de cardápio
+Uma lanchonete digital possui um menu com pratos principais. O chef pediu que fossem adicionados dois novos itens no final da lista para a próxima semana. Os itens são: "hambúrguer artesanal" e "wrap de frango". Atualize o cardápio. */
+
+let cardapio = [
+    "pizza de calabresa",
+    "lasanha",
+    "sushi",
+    "salada Caesar",
+    "pastel",
+    "yakisoba",
+    "feijoada"
+  ];
+  
+  console.log(cardapio);
+  cardapio.push("hambúrguer artesanal", "wrap de frango") // adicionando com push ao final da lista
+
+  console.log("Cardapio atualizado:");
+  console.log(cardapio); // exibinbdo cardapio atualizado
+  
