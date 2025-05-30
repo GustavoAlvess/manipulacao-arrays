292440272bd83191f5ec684329b5c117de1b24ef
GustavoAlvess <gustavo.a.lima21@aluno.senai.br>
2025-05-30 16:19:44 -0300

    feat: resolve exercicio 26

--- scripts/exercicio26.js
+++ scripts/exercicio26.js
@@ -1,1 +1,16 @@
+/* 26 – Histórico de navegação
+A equipe de UX quer analisar o último acesso do usuário e os passos anteriores no sistema. Inverta a ordem do histórico para mostrar o mais recente no topo.
+*/
 
+let historico = [
+  "login",
+  "painel",
+  "relatórios",
+  "detalhes do cliente",
+  "editar dados",
+  "configurações",
+  "logout"
+];
+// usando reverse para inverter a ordem do array
+let historicoInvertido = historico.reverse()
+console.log(historicoInvertido);
