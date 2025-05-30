209fbf376b8945616e4661a477e38b278903d167
GustavoAlvess <gustavo.a.lima21@aluno.senai.br>
2025-05-30 11:59:29 -0300

    feat: resolve exercicio 13

---
+++ scripts/exercicio13.js
@@ -0,0 +1,18 @@
+/* 13 – Relatório de férias: Primeira semana
+Você precisa gerar um relatório com apenas a primeira semana de férias dos colaboradores listados, sem modificar a lista original.
+*/
+
+let diasFaltas = [
+    "Carlos – Segunda",
+    "Carlos – Terça",
+    "Carlos – Quarta",
+    "Carlos – Quinta",
+    "Carlos – Sexta",
+    "Carlos – Segunda (semana 2)",
+    "Carlos – Terça (semana 2)",
+    "Carlos – Quarta (semana 2)"
+  ];
+
+let relatorio = diasFaltas.slice(0, 5)
+
+console.log(" Relatório \n", relatorio);
