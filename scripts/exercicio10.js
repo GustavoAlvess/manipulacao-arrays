d700d5fa8dec14eacb60620e8142a31d0da203fb
GustavoAlvess <gustavo.a.lima21@aluno.senai.br>
2025-05-30 11:47:34 -0300

    feat: resolve exercicio 10

---
+++ scripts/exercicio10.js
@@ -0,0 +1,19 @@
+/* 10 – Verificação de produto em estoque
+Um cliente ligou perguntando se a loja ainda tem "cadeira gamer" disponível. Confira na lista de estoque.
+*/
+
+let estoque = [
+    "mesa para PC",
+    "cadeira gamer",
+    "monitor 27 polegadas",
+    "hub USB",
+    "mouse sem fio"
+  ];
+
+
+if (estoque.includes("cadeira gamer")) {
+    console.log("Sim, a cadeira gamer está disponível");
+    
+} else {
+    console.log("Não, a cadeira gamer não está disponível");
+}
