5feb626808e8c5a06cb435a507af3c6611a636d5
GustavoAlvess <gustavo.a.lima21@aluno.senai.br>
2025-05-30 16:10:18 -0300

    feat: resolve exercicio 23

---
+++ scripts/exercicio23.js
@@ -0,0 +1,17 @@
+/* 23 – Organizar lista de candidatos
+A lista de candidatos para entrevista deve ser organizada em ordem alfabética antes de ser enviada ao RH.
+*/
+
+let candidatos = [
+  "Marcos",
+  "Aline",
+  "Carla",
+  "João",
+  "Fernanda",
+  "Lucas",
+  "Bruno",
+  "Isabela"
+];
+//usando o sort para ordenar em ordem alfabetica
+let candidatosOrdemAlfabetica = candidatos.sort()
+console.log(candidatosOrdemAlfabetica);
