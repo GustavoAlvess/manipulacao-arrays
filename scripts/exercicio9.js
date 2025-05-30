91edd026fa042ba8547aaf87cea7f3e1a77a1feb
GustavoAlvess <gustavo.a.lima21@aluno.senai.br>
2025-05-30 11:41:15 -0300

    feat: resolve exercicio 9

---
+++ scripts/exercicio9.js
@@ -0,0 +1,20 @@
+/*  9 – Conferência de presença
+O RH quer saber se o funcionário "Amanda Souza" compareceu à reunião de alinhamento. Verifique essa informação na lista de presença.
+
+*/
+
+let presenca = [
+    "João Lima",
+    "Carla Nunes",
+    "Amanda Souza",
+    "Bruno Ribeiro",
+    "Fernanda Sales"
+  ];
+
+
+  if( presenca.includes("Amanda Souza")) {
+    console.log("Sim, Amanda Souza está na lista");
+    
+  } else{
+     console.log("Sim, Amanda Souza está na lista");
+  }
