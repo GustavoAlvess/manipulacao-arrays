41495c3a458ffa74654ae5a356a535c8cb582dcf
GustavoAlvess <gustavo.a.lima21@aluno.senai.br>
2025-05-30 11:51:15 -0300

    feat: resolve exercicio 11

---
+++ scripts/exercicio11.js
@@ -0,0 +1,17 @@
+/* 11 – Geração de frase
+Você precisa exibir os ingredientes de uma receita em uma única frase legível para o cliente. Utilize o array de ingredientes.
+*/
+
+let ingredientes = [
+    "farinha de trigo",
+    "fermento",
+    "leite",
+    "ovos",
+    "açúcar",
+    "manteiga"
+  ];
+
+  ingredientes.join() // transformando em string
+
+  console.log(ingredientes);
+  
